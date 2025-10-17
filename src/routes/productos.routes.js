d518067ecce3fc6cce3bf1c87a1a26dab7b520fa
Cristian <diazcristian0210@gmail.com>
2025-10-17 16:07:04 -0300

    configuramos las rutas CRUD para productos en Express

---
+++ src/routes/productos.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { borrarProducto, crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
+const router = Router(); 
+
+//crear las rutas
+router.route('/prueba').get(listarProductos)
+router.route('/productos').post(crearProducto).get(listarProductos);
+router.route('/productos/:id').get(obtenerProducto).put(editarProducto).delete(borrarProducto)
+export default router;
